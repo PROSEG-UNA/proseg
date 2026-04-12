f551a03f565226cba46640d65cc51c10ab322223
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2026-04-11 23:25:05 -0600

    feat: enable config server for centralized configuration management

--- services/msvc-config/src/main/java/com/sssi/msvc_config/MsvcConfigApplication.java
+++ services/msvc-config/src/main/java/com/sssi/msvc_config/MsvcConfigApplication.java
@@ -2,7 +2,10 @@ package com.sssi.msvc_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+// It allows the application to serve configuration properties to other microservices in a centralized manner.
+@EnableConfigServer
 @SpringBootApplication
 public class MsvcConfigApplication {
 
