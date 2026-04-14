bf08a331a3ec7b7c06a968fbf734d6ff73451bfc
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2026-04-14 17:01:45 -0600

    feat(msvc-auth): rename request classes and introduce response DTOs for login and registration

--- services/msvc-auth/src/main/java/com/sssi/msvc_auth/dto/RegisterResponseDto.java
+++ services/msvc-auth/src/main/java/com/sssi/msvc_auth/dto/RegisterResponseDto.java
@@ -1,17 +1,16 @@
 package com.sssi.msvc_auth.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class AuthResponse {
+public class RegisterResponseDto {
     private String token;
     private String username;
     private String email;
-    private String message;
-    private boolean success;
 }
-
