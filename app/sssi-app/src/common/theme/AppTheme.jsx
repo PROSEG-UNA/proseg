3a1312d0563e0076549ae61b662221a588307f74
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-04-16 19:07:35 -0600

    feat(theme): expand MUI customizations and add color mode support

--- app/sssi-app/src/common/theme/AppTheme.jsx
+++ app/sssi-app/src/common/theme/AppTheme.jsx
@@ -5,37 +5,39 @@ import { dataDisplayCustomizations } from './customizations/dataDisplay';
 import { feedbackCustomizations } from './customizations/feedback';
 import { navigationCustomizations } from './customizations/navigation';
 import { surfacesCustomizations } from './customizations/surfaces';
+import { dataGridCustomizations } from './customizations/dataGrid';
 import { colorSchemes, typography, shadows, shape } from './themePrimitives';
 
 export default function AppTheme({ children, disableCustomTheme, themeComponents, defaultColorScheme = 'light' }) {
-  const theme = React.useMemo(() => {
-    return disableCustomTheme
-      ? {}
-      : createTheme({
-          cssVariables: {
-            colorSchemeSelector: 'data-mui-color-scheme',
-            cssVarPrefix: 'template',
-          },
-          colorSchemes,
-          typography,
-          shadows,
-          shape,
-          components: {
-            ...inputsCustomizations,
-            ...dataDisplayCustomizations,
-            ...feedbackCustomizations,
-            ...navigationCustomizations,
-            ...surfacesCustomizations,
-            ...themeComponents,
-          },
-        });
-  }, [disableCustomTheme, themeComponents]);
-  if (disableCustomTheme) {
-    return <React.Fragment>{children}</React.Fragment>;
-  }
-  return (
-    <ThemeProvider theme={theme} defaultMode={defaultColorScheme} disableTransitionOnChange>
-      {children}
-    </ThemeProvider>
-  );
+    const theme = React.useMemo(() => {
+        return disableCustomTheme
+            ? {}
+            : createTheme({
+                cssVariables: {
+                    colorSchemeSelector: 'data-mui-color-scheme',
+                    cssVarPrefix: 'template',
+                },
+                colorSchemes,
+                typography,
+                shadows,
+                shape,
+                components: {
+                    ...inputsCustomizations,
+                    ...dataDisplayCustomizations,
+                    ...feedbackCustomizations,
+                    ...navigationCustomizations,
+                    ...surfacesCustomizations,
+                    ...dataGridCustomizations,
+                    ...themeComponents,
+                },
+            });
+    }, [disableCustomTheme, themeComponents]);
+    if (disableCustomTheme) {
+        return <React.Fragment>{children}</React.Fragment>;
+    }
+    return (
+        <ThemeProvider theme={theme} defaultMode={defaultColorScheme} disableTransitionOnChange>
+            {children}
+        </ThemeProvider>
+    );
 }
