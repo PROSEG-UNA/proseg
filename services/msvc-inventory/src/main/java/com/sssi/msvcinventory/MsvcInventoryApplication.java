7657fce4f47412256f92b928333f83b541b60d0f
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-04-27 19:00:51 -0600

    feat(msvc-inventory): initial microservice setup

---
+++ services/msvc-inventory/src/main/java/com/sssi/msvcinventory/MsvcInventoryApplication.java
@@ -0,0 +1,15 @@
+package com.sssi.msvcinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsvcInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcInventoryApplication.class, args);
+    }
+
+}
---
+++ services/msvc-inventory/src/main/java/com/sssi/msvcinventory/repository/InventoryRepository.java
@@ -0,0 +1,4 @@
+package com.sssi.msvcinventory.repository;
+
+public interface InventoryRepository {
+}
