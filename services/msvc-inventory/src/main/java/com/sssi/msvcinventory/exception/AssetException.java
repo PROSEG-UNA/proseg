324323b20c89c38ad497e2230e0fbeea6925ad32
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-05-01 17:59:08 -0600

    feat(inventory): add exceptions for asset management

---
+++ services/msvc-inventory/src/main/java/com/sssi/msvcinventory/exception/AssetException.java
@@ -0,0 +1,27 @@
+package com.sssi.msvcinventory.exception;
+
+import com.sssi.common.api.exception.BaseException;
+import org.springframework.http.HttpStatus;
+
+public class AssetException extends BaseException {
+
+    public AssetException(HttpStatus status, String errorCode, String message) {
+        super(status, errorCode, message);
+    }
+
+    public static AssetException notFound(String id) {
+        return new AssetException(
+                HttpStatus.NOT_FOUND,
+                "ASSET_NOT_FOUND",
+                "Activo no encontrado con id: " + id
+        );
+    }
+
+    public static AssetException networkInterfaceRequired(String assetTypeName) {
+        return new AssetException(
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                "ASSET_NETWORK_INTERFACE_REQUIRED",
+                "El tipo de activo '" + assetTypeName + "' requiere una interfaz de red"
+        );
+    }
+}
