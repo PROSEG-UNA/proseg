0b59a541d05ccf2424f32e27876743c49bd6af05
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2026-05-08 00:19:44 -0600

    feat(security): add DTOs for password reset and invitation functionalities

---
+++ services/msvc-auth/src/main/java/com/sssi/msvc_auth/dto/InvitationInfoResponseDto.java
@@ -0,0 +1,15 @@
+package com.sssi.msvc_auth.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.Instant;
+
+@Data
+@Builder
+public class InvitationInfoResponseDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Instant expiresAt;
+}
