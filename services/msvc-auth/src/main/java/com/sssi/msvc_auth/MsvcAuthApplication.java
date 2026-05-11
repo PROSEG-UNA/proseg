74a23fd4fcf297b7be3126420907b6edf2ebde5c
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2026-05-10 23:56:20 -0600

    feat(application): enable scheduling for password expiration notifications

--- services/msvc-auth/src/main/java/com/sssi/msvc_auth/MsvcAuthApplication.java
+++ services/msvc-auth/src/main/java/com/sssi/msvc_auth/MsvcAuthApplication.java
@@ -3,9 +3,9 @@ package com.sssi.msvc_auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-// That service is going to be a client of Eureka,
-// so we need to add the annotation @EnableDiscoveryClient
+@EnableScheduling
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MsvcAuthApplication {
