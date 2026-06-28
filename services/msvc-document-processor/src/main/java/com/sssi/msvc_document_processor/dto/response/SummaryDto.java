a34fa813372ec76c5d4849c150030de2f165ca31
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-06-28 13:51:21 -0600

    feat(document-processor): define generic schema and preview/summary DTOs

---
+++ services/msvc-document-processor/src/main/java/com/sssi/msvc_document_processor/dto/response/SummaryDto.java
@@ -0,0 +1,22 @@
+package com.sssi.msvc_document_processor.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SummaryDto {
+    private int totalRows;
+
+    private int created;
+
+    private boolean cancelled;
+
+    private List<RowIssueDto> errors;
+
+    private List<RowIssueDto> warnings;
+}
