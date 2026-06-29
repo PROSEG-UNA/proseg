c8ca3c095f96ab1cfff915c7a3f43e27656579ac
AlejandroMirandaGomez <alejandro.obed.miranda@gmail.com>
2026-06-28 18:57:15 -0600

    feat(document-processor): define import strategy contract and inventory communication DTOs

---
+++ services/msvc-document-processor/src/main/java/com/sssi/msvc_document_processor/dto/response/ImportConfirmDto.java
@@ -0,0 +1,22 @@
+package com.sssi.msvc_document_processor.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImportConfirmDto {
+    private int received;
+
+    private int created;
+
+    private boolean cancelled;
+
+    private List<RowIssueDto> errors;
+
+    private List<RowIssueDto> warnings;
+}
---
+++ services/msvc-document-processor/src/main/java/com/sssi/msvc_document_processor/dto/response/ImportPreviewDto.java
@@ -0,0 +1,19 @@
+package com.sssi.msvc_document_processor.dto.response;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImportPreviewDto {
+
+    private int received;
+
+    private List<RowIssueDto> errors;
+
+    private List<PendingCreationDto> pendingCreations;
+}
